ad5b5741234efbc43c41f6f0da5a45ad4bd147a4
Jirka Kremser <jiri.kremser@gmail.com>
2026-06-02 13:43:03 +0200

    first shot
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+helm upgrade -i ingress-nginx ingress-nginx --reuse-values \
+   --repo https://kubernetes.github.io/ingress-nginx \
+   --namespace ingress-nginx \
+   --set controller.metrics.enabled=true
+
+#k port-forward svc/ingress-nginx-controller-metrics 10254 -ningress-nginx
+#open http://localhost:10254/metrics
+
+# nginx_ingress_controller_connect_duration_seconds_count{canary="",controller_class="k8s.io/ingress-nginx",controller_namespace="ingress-nginx",controller_pod="ingress-nginx-controller-d85848749-8z66x",host="msol-app-fe-main.local",ingress="msol-app-fe-main",method="GET",namespace="app",path="/",service="msol-app-fe-main",status="200"} 1
+
+# k port-forward -ningress-nginx service/ingress-nginx-controller 8080:80
+# curl --resolve msol-app-fe-main.local:8080:127.0.0.1 http://msol-app-fe-main.local:8080
+
+helm upgrade -i keda-otel-scaler -nkeda \
+ --repo oci://ghcr.io/kedify/charts/otel-add-on \
+ --version=v0.1.3 \
+ -f ./otel-scaler-values.yaml
+
+k apply -f fe.yaml -f be-so.yaml -f fe-so.yaml
+
+echo -e "continue w/:\nhey -c 100 -z 60s -t 90 -host msol-app-fe-main.local http://localhost:8080"
+#hey -c 100 -z 60s -t 90 -host msol-app-fe-main.local http://localhost:8080
